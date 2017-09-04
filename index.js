57f026ea86220774fd0088a5a832c034992bc642
vgoreiko <vgoreiko@deloitte.com>
2017-09-04 17:53:51 +0300

    added some properties

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ let mainWindow;
 
 app.on('ready', ()=> {
     mainWindow = new BrowserWindow({
-        show: false
+        show: false,
+        width: 400,
+        height: 400
     });
     mainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
     mainWindow.on('ready-to-show', ()=> {
         mainWindow.show();
+    })
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
     })
 })
