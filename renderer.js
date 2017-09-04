85b8ce8001353d18bc0a7414c9ee0be174e87194
vgoreiko <vgoreiko@deloitte.com>
2017-09-04 17:38:12 +0300

    added click event

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 const versionEl = document.querySelector('#version');
 versionEl.innerText = process.versions.electron;
 console.log(process.versions);
+
+const btn = document.querySelector('#toClickBtn');
+btn.addEventListener('click', clicked)
+
+function clicked(){
+    alert('bla')
+}
